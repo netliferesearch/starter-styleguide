a7c9b75bb904e0b84f78c2b461009e68d50bd2a2
Fredrik Jensen <fredrik@netliferesearch.com>
2016-02-29 21:03:36 +0100

    added styleguide.css

--- config.js
+++ config.js
@@ -1,7 +1,10 @@
 module.exports = {
     source: 'src/scss',
     destination: 'dist/styleguide/',
-    css: '../css/main.min.css',
+    css: [
+        '../css/main.min.css',
+        '..css/styleguide.min.css'
+    ]
     homepage: 'index.md',
     placeholder: '{modifier}',
     title: 'Styleguide',
